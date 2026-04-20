56f1f706d420541c14e662c9f38903b3315f4739
summi-rahman <tahseenanwer100@gmail.com>
2026-04-20 16:00:29 +0530

    Connecting Frontend And Backend

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React,{useEffect ,useState} from 'react'
 
 function App() {
+  const [serverStatus, setServerStatus] = useState("connecting to Kitchen (backend)....");
+  useEffect(()=>{
+    fetch('http://localhost:5002/api')
+    .then((res)=> res.text())
+    .then((data)=>{
+      setServerStatus(data);
+    })
+    .catch((err)=>{
+      setServerStatus("Kitchen is closed (Server error)");
+      console.log(err);
+    })
+  },[])
   return (
-    <div className='text-6xl'>
-     <h1>Welcome to the Home page </h1>
+    <div className='flex flex-col items-center justify-center h-screen bg-zinc-900 text-white'>
+      <h1 className='text-6xl font-bold mb-4'>Cognite-AI</h1>
+      <p className='text-xl text-green-400'>{serverStatus}</p>
     </div>
   )
 }
--- Server/src/Index.js
+++ Server/src/Index.js
@@ -1,9 +1,29 @@
 import express from 'express'
+import cors from 'cors';
+
 
 const app = express();
- PORT = 5001;
-app.listen(`Server is listening on http://localhost:${PORT}`);
+const PORT = process.env.PORT || 5002
+
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/api', (req,res)=>{
   res.send('Cognito AI Kitchen is Open!');
 })
+
+app.post('/api/test', (req,res)=>{
+  const ReceivedData = req.body;
+  console.log("Data Received from the user is " , ReceivedData);
+  res.json({
+    status:"Success",
+    message:"Server Received your Data",
+    data:ReceivedData
+  });
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
